10def411254c60eb3b7a9761437fe640ec808497
Hibri Marzook <hibri@hibri.net>
2019-07-23 09:51:03 +0100

    delete

--- aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias ipcalc='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hibri/ipcalc:latest $*'
-alias openssl='docker run --rm -v ${HOME}:/root -v `pwd`:/work -w /work -it hibri/openssl:latest $*'
